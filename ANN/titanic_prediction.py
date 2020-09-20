01c47eaaf1f220bda6fd70cded8af0f735a14fbe
Sangsaka Wira <sangsakawira@gmail.com>
2020-09-20 17:46:21 +0700

    CNN fashion mnist reviewed

---
+++ ANN/titanic_prediction.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from tensorflow.keras.layers import Dense, Flatten
+from sklearn.compose import ColumnTransformer
+from tensorflow.keras.models import Sequential
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
